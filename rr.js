ff3811f4c31e40b1baa306f39d669c5d4f5b6beb
imswer <saiteja.korrayi@gmail.com>
2024-11-13 14:12:23 +0530

    no message

---
+++ rr.js
@@ -0,0 +1,109 @@
+// script.js
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const playButton = document.getElementById('playButton');
+const levelSelect = document.getElementById('levelSelect');
+const markXButton = document.getElementById('markXButton');
+const markOButton = document.getElementById('markOButton');
+
+let currentClass = X_CLASS;
+let circleTurn;
+
+playButton.addEventListener('click', () => {
+    board.style.visibility = 'visible';
+    startGame();
+});
+
+restartButton.addEventListener('click', startGame);
+
+markXButton.addEventListener('click', () => {
+    currentClass = X_CLASS;
+    setBoardHoverClass();
+});
+
+markOButton.addEventListener('click', () => {
+    currentClass = CIRCLE_CLASS;
+    setBoardHoverClass();
+});
+
+function startGame() {
+    circleTurn = false;
+    currentClass = X_CLASS;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(CIRCLE_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!';
+    } else {
+        winningMessageTextElement.innerText = `${currentClass === X_CLASS ? "X's" : "O's"} Wins!`;
+    }
+    winningMessageElement.classList.add('show');
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if (currentClass === X_CLASS) {
+        board.classList.add(X_CLASS);
+    } else {
+        board.classList.add(CIRCLE_CLASS);
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
+}
